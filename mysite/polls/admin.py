e724c1160c942dbb063938406964bc4c826c5430
olopatynska <olopatynska@activewizards.com>
2018-02-22 12:48:14 +0200

    added models with different types of connections, they were tested in admin

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -3,7 +3,13 @@ from __future__ import unicode_literals
 
 from django.contrib import admin
 
-from .models import User, Code
+from .models import User, Code, Manufacturer, Car, Topping, Pizza, Place, Restaurant
 
 admin.site.register(User)
 admin.site.register(Code)
+admin.site.register(Manufacturer)
+admin.site.register(Car)
+admin.site.register(Topping)
+admin.site.register(Pizza)
+admin.site.register(Place)
+admin.site.register(Restaurant)
--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -14,3 +14,53 @@ class User(models.Model):
 
 class Code(models.Model):
     zipcode = models.CharField(max_length=5)
+
+#Many to one connection
+class Manufacturer(models.Model):
+	name = models.CharField(max_length=20)
+	country = models.CharField(max_length=20)
+
+	def __unicode__(self):
+		return self.name
+
+class Car(models.Model):
+	manufacturer = models.ForeignKey(Manufacturer, on_delete=models.CASCADE)
+	name = models.CharField(max_length=20)
+
+	def __unicode__(self):
+		return self.name
+
+#Many to many connection
+class Topping(models.Model):
+    name = models.CharField(max_length=20)
+    mass = models.IntegerField()
+
+    def __unicode__(self):
+		return self.name
+
+class Pizza(models.Model):
+    toppings = models.ManyToManyField(Topping)
+    name = models.CharField(max_length=20)
+
+    def __unicode__(self):
+		return self.name
+
+# one to one connection
+class Place(models.Model):
+    name = models.CharField(max_length=50)
+    address = models.CharField(max_length=80)
+
+    def __unicode__(self):              
+        return self.name
+
+class Restaurant(models.Model):
+    place = models.OneToOneField(
+        Place,
+        on_delete=models.CASCADE,
+        primary_key=True,
+    )
+    serves_hot_dogs = models.BooleanField(default=False)
+    serves_pizza = models.BooleanField(default=False)
+
+    def __unicode__(self):
+    	return self.place.name
